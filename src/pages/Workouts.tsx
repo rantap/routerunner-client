92e0d2897eecccd234448965389b735ad1bf085c
Petri <peewter@gmail.com>
2024-01-07 19:18:07 +0200

    Add template for workout log page

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,4 +1,10 @@
 const Workouts = () => {
-  return <h1>Your workouts</h1>;
+  return (
+    <div>
+      <h1 className='mt-4 text-center text-slate-100 text-xl font-bold tracking-tighter'>
+        Your workouts
+      </h1>
+    </div>
+  );
 };
 export default Workouts;
