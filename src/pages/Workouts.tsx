efcd6db8c1e326c60261c69a2ebe66eca03eeeb0
Petri <peewter@gmail.com>
2024-01-19 10:56:45 +0200

    Add components to Workouts page

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,10 +1,28 @@
+import AddWorkoutForm from '../components/AddWorkoutForm';
+import WorkoutList from '../components/WorkoutList';
+import WorkoutModal from '../components/WorkoutModal';
+// import WorkoutTable from '../components/WorkoutTable';
+
 const Workouts = () => {
   return (
-    <div>
-      <h1 className='mt-4 text-center text-slate-100 text-xl font-bold tracking-tighter'>
-        Your workouts
-      </h1>
-    </div>
+    <>
+      <div>
+        <h1 className='mt-4 text-center text-slate-100 text-xl font-bold tracking-tighter'>
+          Your workouts
+        </h1>
+      </div>
+      <div className='mt-4 flex justify-center sm:hidden'>
+        <WorkoutModal />
+      </div>
+      <div className='flex flex-wrap mt-4'>
+        <div className='mx-auto mt-4 w-5/6 sm:w-2/5'>
+          <WorkoutList />
+        </div>
+        <div className='hidden mx-auto mt-10 w-5/6 sm:w-2/5 sm:block'>
+          <AddWorkoutForm />
+        </div>
+      </div>
+    </>
   );
 };
 export default Workouts;
