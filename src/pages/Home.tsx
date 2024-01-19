f36a8a6e5ec5bfe338374efd06a980292c6099d6
Petri <peewter@gmail.com>
2024-01-19 10:56:08 +0200

    Increase font size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className='m-4 text-center text-slate-100 text-xl font-bold tracking-tighter'>
         Create a route
       </h1>
-      <p className='mx-4 mb-6 text-sm text-center text-slate-100'>
+      <p className='mx-6 mb-6 text-center text-slate-100'>
         Plan your own route by creating waypoints on the map. Touch/click the desired spot to create
         a waypoint. Touch/click a waypoint to remove it.
       </p>
