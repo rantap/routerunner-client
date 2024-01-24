f6d6f686e76de2117c12b6a705eb3e20ba4fa93b
Petri <peewter@gmail.com>
2024-01-24 18:29:50 +0200

    Add utility function for distance formatting

---
+++ src/utils/formatDistance.tsx
@@ -0,0 +1,4 @@
+export const formatDistance = (totalDistance: number) => {
+  const distance: number = Math.trunc(totalDistance);
+  return distance < 1000 ? <span>{distance} m</span> : <span>{distance / 1000} km</span>;
+};
