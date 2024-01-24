38bf15cfdf627c3a622d6486f954bd9c5ef5e199
Petri <peewter@gmail.com>
2024-01-24 14:35:56 +0200

    Update modal to use controlled open state

--- src/components/WorkoutModal.tsx
+++ src/components/WorkoutModal.tsx
@@ -1,58 +1,31 @@
-import {
-  Button,
-  Dialog,
-  DialogTrigger,
-  FieldError,
-  Form,
-  Input,
-  Label,
-  Modal,
-  TextField,
-} from 'react-aria-components';
+import { useState } from 'react';
+import { Button, Dialog, DialogTrigger, Modal, ModalOverlay } from 'react-aria-components';
+import AddWorkoutForm from './AddWorkoutForm';
 
 const WorkoutModal = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <DialogTrigger>
-        <Button className='mt-4 p-2 bg-green-300 rounded'>+ Add workout</Button>
-        <Modal isDismissable>
-          <Dialog>
-            {({ close }) => (
-              <Form
-                className='fixed top-32 right-0 left-0 mx-auto mt-4 w-5/6 p-6 bg-zinc-800 rounded-lg text-left sm:static'
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  alert(JSON.stringify(Object.fromEntries(new FormData(e.currentTarget))));
-                  close();
-                }}
-              >
-                <TextField className='mb-4 mt-2' name='type' type='type' isRequired>
-                  <Label className='text-slate-100 mr-2'>Workout type</Label>
-                  <Input className='mt-1 p-1 rounded w-full' />
-                  <FieldError className='text-red-500 text-sm' />
-                </TextField>
-                <TextField className='mb-4 mt-2' name='date' type='date' isRequired>
-                  <Label className='text-slate-100'>Date</Label>
-                  <Input className='mt-1 p-1 rounded w-full' />
-                  <FieldError className='text-red-500 text-sm' />
-                </TextField>
-                <TextField className='mb-4 mt-2' name='distance' type='distance' isRequired>
-                  <Label className='text-slate-100'>Distance</Label>
-                  <Input className='mt-1 p-1 rounded w-full' />
-                  <FieldError className='text-red-500 text-sm' />
-                </TextField>
-                <TextField className='mt-2' name='duration' type='duration' isRequired>
-                  <Label className='text-slate-100'>Duration</Label>
-                  <Input className='mt-1 p-1 rounded w-full' />
-                  <FieldError className='text-red-500 text-sm' />
-                </TextField>
-                <Button className='flex mt-8 px-8 py-2  bg-green-300 rounded mx-auto' type='submit'>
-                  + Add workout
-                </Button>
-              </Form>
-            )}
-          </Dialog>
-        </Modal>
+        <Button
+          onPress={() => setOpen(true)}
+          className='mt-4 px-8 py-2  bg-green-300 rounded-full transition ease-in-out hover:scale-105 hover:bg-green-400 duration-300'
+        >
+          + Add workout
+        </Button>
+        <ModalOverlay
+          isDismissable
+          isOpen={isOpen}
+          onOpenChange={setOpen}
+          className='fixed inset-0 backdrop-blur'
+        >
+          <Modal>
+            <Dialog className='fixed top-24 right-0 left-0 mx-auto mt-4 w-5/6 p-2 bg-zinc-800 rounded-lg text-left drop-shadow-xl'>
+              <AddWorkoutForm />
+            </Dialog>
+          </Modal>
+        </ModalOverlay>
       </DialogTrigger>
     </>
   );
