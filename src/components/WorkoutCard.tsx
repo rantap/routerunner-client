bd299ec039e77ed8b2c83e7b0655af56b66fdd34
Petri <peewter@gmail.com>
2024-01-24 14:34:17 +0200

    Add chevron icons and adjust buttons

--- src/components/WorkoutCard.tsx
+++ src/components/WorkoutCard.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Button } from 'react-aria-components';
+import { ChevronUpIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
 interface Workout {
   id: number;
@@ -8,8 +10,8 @@ interface Workout {
   length: string;
   time: number;
 }
-const WorkoutCard = ({ id, type, date, length, time }: Workout) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+const WorkoutCard = ({ type, date, length, time }: Workout) => {
+  const [isExpanded, setExpanded] = useState(false);
 
   return (
     <>
@@ -23,17 +25,25 @@ const WorkoutCard = ({ id, type, date, length, time }: Workout) => {
       </p>
       <div
         className='flex flex-wrap justify-between p-4 bg-zinc-800 text-slate-100 rounded-lg hover:cursor-pointer'
-        onClick={() => setIsExpanded(!isExpanded)}
+        onClick={() => setExpanded(!isExpanded)}
       >
-        <p>{id}</p>
+        {isExpanded ? (
+          <ChevronUpIcon className='mt-1 h-5 w-5 transition-all duration-300 ease-in' />
+        ) : (
+          <ChevronDownIcon className='mt-1 h-5 w-5 animation-spin' />
+        )}
         <p>{type}</p>
         <p>{length} km</p>
         <p>{new Date(time * 1000).toISOString().slice(11, 19)}</p>
       </div>
       {isExpanded && (
-        <div className=''>
-          <Button className='mt-4 py-2 px-6 mr-8 bg-red-700 text-black rounded'>Delete</Button>
-          <Button className='mt-4 py-2 px-6 bg-yellow-300 text-black rounded'>Edit</Button>
+        <div className='flex justify-center transition duration-1000 ease-in-out'>
+          <Button className='mt-4 py-2 px-6 mr-4 bg-red-400 text-black rounded-full transition ease-in-out hover:scale-105 hover:bg-red-500 duration-300'>
+            Delete
+          </Button>
+          <Button className='mt-4 py-2 px-6 bg-yellow-300 text-black rounded-full transition ease-in-out hover:scale-105 hover:bg-yellow-400 duration-300'>
+            Edit
+          </Button>
         </div>
       )}
     </>
