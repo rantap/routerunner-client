65057efea462bc6262fe847bf9bdd2afab991db9
Petri <peewter@gmail.com>
2024-01-25 12:39:15 +0200

    Remove ModalOverlay

--- src/components/WorkoutModal.tsx
+++ src/components/WorkoutModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Dialog, DialogTrigger, Modal, ModalOverlay } from 'react-aria-components';
+import { Button, Dialog, DialogTrigger, Modal } from 'react-aria-components';
 import AddWorkoutForm from './AddWorkoutForm';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
@@ -14,20 +14,18 @@ const WorkoutModal = () => {
       >
         + Add workout
       </Button>
-      <ModalOverlay
+      <Modal
         isOpen={isOpen}
         onOpenChange={setOpen}
-        className='fixed inset-0 bg-zinc-900 backdrop-blur-none overflow-y-auto'
+        className='fixed top-24 inset-0 mt-4 w-full text-left bg-zinc-900 backdrop-blur-none overflow-y-auto'
       >
-        <Modal className='fixed top-24 mt-4 w-full text-left'>
-          <Dialog>
-            <Button className='fixed right-0 my-2 mx-4 sm:hidden' onPress={() => setOpen(false)}>
-              <XMarkIcon className='h-10 w-10 text-slate-100' />
-            </Button>
-            <AddWorkoutForm />
-          </Dialog>
-        </Modal>
-      </ModalOverlay>
+        <Dialog>
+          <Button className='fixed right-0 my-2 mx-4 sm:hidden' onPress={() => setOpen(false)}>
+            <XMarkIcon className='h-10 w-10 text-slate-100' />
+          </Button>
+          <AddWorkoutForm />
+        </Dialog>
+      </Modal>
     </DialogTrigger>
   );
 };
