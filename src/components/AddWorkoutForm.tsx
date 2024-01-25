afdcd64bc126eccc206c84dc1e9493a34de5f7b3
Petri <peewter@gmail.com>
2024-01-25 12:17:45 +0200

    Adjust inputs

--- src/components/AddWorkoutForm.tsx
+++ src/components/AddWorkoutForm.tsx
@@ -3,30 +3,33 @@ import { Form, TextField, Label, Input, Button, FieldError } from 'react-aria-co
 const AddWorkoutForm = () => {
   return (
     <Form
-      className='p-4 bg-zinc-800 rounded-lg text-left'
+      className='p-4 mb-10 bg-zinc-800 rounded-3xl text-left'
       onSubmit={(e) => {
         e.preventDefault();
         alert(JSON.stringify(Object.fromEntries(new FormData(e.currentTarget))));
       }}
     >
-      <TextField className='mb-4 mt-2' name='type' type='type' isRequired>
-        <Label className='ml-2 text-slate-100 mr-2'>Workout type</Label>
-        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-full border  border-slate-100/20 w-full' />
+      <div className='flex justify-start'>
+        <h1 className='ml-2 text-slate-100 text-xl font-bold tracking-tighter'>Add workout</h1>
+      </div>
+      <TextField className='mb-4 mt-6' name='type' type='type' isRequired>
+        <Label className='ml-2 text-slate-100'>Workout type</Label>
+        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-lg outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300' />
         <FieldError className='ml-2 text-red-500' />
       </TextField>
       <TextField className='mb-4 mt-2' name='date' type='date' isRequired>
         <Label className='ml-2 text-slate-100'>Date</Label>
-        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-full border  border-slate-100/20 w-full' />
+        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-lg outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300' />
         <FieldError className='ml-2 text-red-500' />
       </TextField>
       <TextField className='mb-4 mt-2' name='distance' type='distance' isRequired>
         <Label className='ml-2 text-slate-100'>Distance</Label>
-        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-full border  border-slate-100/20 w-full' />
+        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-lg outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300' />
         <FieldError className='ml-2 text-red-500' />
       </TextField>
       <TextField className='mt-2' name='duration' type='duration' isRequired>
         <Label className=' ml-2 text-slate-100'>Duration</Label>
-        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-full border  border-slate-100/20 w-full' />
+        <Input className='mt-1 px-2 py-2 bg-zinc-900 text-slate-100 rounded-lg outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300' />
         <FieldError className='ml-2 text-red-500' />
       </TextField>
       <Button
