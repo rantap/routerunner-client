b14b35bc6aad45e447c91f250dea838a6c820f51
Petri <peewter@gmail.com>
2024-01-25 12:16:39 +0200

    Separate tabs into designated component

--- src/components/WorkoutList.tsx
+++ src/components/WorkoutList.tsx
@@ -1,4 +1,3 @@
-import { Tabs, TabList, Tab, TabPanel } from 'react-aria-components';
 import WorkoutCard from './WorkoutCard';
 
 const WorkoutList = () => {
@@ -27,37 +26,18 @@ const WorkoutList = () => {
   ];
 
   return (
-    <Tabs className='text-slate-300'>
-      <TabList className='flex justify-around mb-4 ' aria-label='Workouts'>
-        <Tab
-          className='p-2 border-solid border-2 border-transparent hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100'
-          id='All'
-        >
-          All workouts
-        </Tab>
-        <Tab
-          className='p-2 border-solid border-2 border-transparent hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100'
-          id='Totals'
-        >
-          Totals
-        </Tab>
-      </TabList>
-      <TabPanel id='All'>
-        {workouts.map((workout) => (
-          <WorkoutCard
-            key={workout.id}
-            id={workout.id}
-            type={workout.type}
-            date={workout.date}
-            length={workout.length}
-            time={workout.time}
-          />
-        ))}
-      </TabPanel>
-      <TabPanel id='Totals'>
-        <p className='text-slate-100'>Total calculations</p>
-      </TabPanel>
-    </Tabs>
+    <>
+      {workouts.map((workout) => (
+        <WorkoutCard
+          key={workout.id}
+          id={workout.id}
+          type={workout.type}
+          date={workout.date}
+          length={workout.length}
+          time={workout.time}
+        />
+      ))}
+    </>
   );
 };
 export default WorkoutList;
---
+++ src/components/WorkoutTabs.tsx
@@ -0,0 +1,30 @@
+import { Tab, TabList, TabPanel, Tabs } from 'react-aria-components';
+import WorkoutList from './WorkoutList';
+
+const WorkoutTabs = () => {
+  return (
+    <Tabs className='text-slate-300'>
+      <TabList className='flex justify-around mb-4 ' aria-label='Workouts'>
+        <Tab
+          className='p-2 border-solid border-2 border-transparent hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100'
+          id='All'
+        >
+          All workouts
+        </Tab>
+        <Tab
+          className='p-2 border-solid border-2 border-transparent hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100'
+          id='Totals'
+        >
+          Totals
+        </Tab>
+      </TabList>
+      <TabPanel id='All'>
+        <WorkoutList />
+      </TabPanel>
+      <TabPanel id='Totals'>
+        <p className='text-slate-100'>Total calculations</p>
+      </TabPanel>
+    </Tabs>
+  );
+};
+export default WorkoutTabs;
