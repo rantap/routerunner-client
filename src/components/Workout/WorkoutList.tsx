8dbd5524ef992c74ffbd89064697ac61879fb0d7
Petri <peewter@gmail.com>
2024-02-01 12:09:28 +0200

    Get all workouts and pass props to Card component

--- src/components/Workout/WorkoutList.tsx
+++ src/components/Workout/WorkoutList.tsx
@@ -1,54 +1,21 @@
-//import { useFetch } from '../../hooks/useFetch';
+import { useQuery } from '@tanstack/react-query';
+import { Workout } from '../../types';
+import { fetchWorkouts } from '../../api/workouts';
 import WorkoutCard from './WorkoutCard';
 
-interface Workout {
-  id: number;
-  type: string;
-  date: string;
-  length: string;
-  time: number;
-}
-
 const WorkoutList = () => {
-  const workouts = [
-    {
-      id: 1,
-      type: 'Running',
-      date: '2023-10-05T14:48:00.000Z',
-      length: '15.02',
-      time: 3600,
-    },
-    {
-      id: 2,
-      type: 'Walking',
-      date: '2023-10-05T14:48:00.000Z',
-      length: '15.02',
-      time: 3600,
-    },
-    {
-      id: 3,
-      type: 'Running',
-      date: '2023-10-05T14:48:00.000Z',
-      length: '15.02',
-      time: 2400,
-    },
-  ];
-  /* const { data, loading, error } = useFetch<Workout[]>('http://localhost:3001/workouts');
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['workouts'],
+    queryFn: fetchWorkouts,
+  });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>; */
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <>
-      {workouts?.map((workout: Workout) => (
-        <WorkoutCard
-          key={workout.id}
-          id={workout.id}
-          type={workout.type}
-          date={workout.date}
-          length={workout.length}
-          time={workout.time}
-        />
+      {data?.map((workout: Workout) => (
+        <WorkoutCard key={workout.id} workout={workout} />
       ))}
     </>
   );
