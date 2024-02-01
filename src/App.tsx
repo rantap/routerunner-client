8ca4ae93e17ff4df5e5c48c04e961ccd5d7f3cc6
Petri <peewter@gmail.com>
2024-02-01 12:06:08 +0200

    Use react-query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Workouts from './pages/Workouts';
 import Routeplanner from './pages/Routeplanner';
 import Navbar from './components/Header/Navbar';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Workouts />} />
-        <Route path='/routeplanner' element={<Routeplanner />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Workouts />} />
+          <Route path='/routeplanner' element={<Routeplanner />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
