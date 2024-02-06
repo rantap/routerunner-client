d54a43c845f817ee2f206b18514d1ad0a4dde6ad
Petri <peewter@gmail.com>
2024-02-06 18:47:55 +0200

    Rearrange components in workouts page

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,24 +1,20 @@
-import AddWorkoutForm from '../components/Workout/Forms/AddWorkoutForm';
-import WorkoutModal from '../components/Workout/WorkoutModal';
+import AddModal from '../components/Workout/AddModal';
 import WorkoutTabs from '../components/Workout/WorkoutTabs';
 
 const Workouts = () => {
   return (
     <div className='mb-10 xl:mx-20'>
       <div>
-        <h1 className='mt-6 text-center text-slate-100 text-2xl font-bold tracking-tighter'>
+        <h1 className='mt-6 text-center text-slate-100 text-2xl font-bold tracking-tighter sm:mb-6'>
           Your workouts
         </h1>
       </div>
-      <div className='flex justify-center sm:hidden'>
-        <WorkoutModal />
+      <div className='flex justify-center'>
+        <AddModal />
       </div>
       <div className='flex flex-wrap'>
-        <div className='mx-auto mt-4 w-5/6 sm:w-2/5'>
+        <div className='mx-auto mt-4 w-5/6 sm:w-3/5'>
           <WorkoutTabs />
-        </div>
-        <div className='hidden mx-auto mt-8 w-5/6 sm:w-2/5 sm:block'>
-          <AddWorkoutForm />
         </div>
       </div>
     </div>
