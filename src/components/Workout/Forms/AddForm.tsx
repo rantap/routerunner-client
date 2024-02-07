539cea3a21202e6f11866e960b031c2b4b9d78d9
Petri <peewter@gmail.com>
2024-02-07 18:20:42 +0200

    Add date picker

--- src/components/Workout/Forms/AddForm.tsx
+++ src/components/Workout/Forms/AddForm.tsx
@@ -1,26 +1,10 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import {
-  Form,
-  TextField,
-  Label,
-  Input,
-  Button,
-  FieldError,
-  DateInput,
-  DateSegment,
-  Calendar,
-  CalendarCell,
-  CalendarGrid,
-  DatePicker,
-  Dialog,
-  Group,
-  Heading,
-  Popover,
-} from 'react-aria-components';
+import { Form, TextField, Label, Input, Button, FieldError } from 'react-aria-components';
 import { useForm, Controller } from 'react-hook-form';
+import { today, getLocalTimeZone, toCalendarDateTime } from '@internationalized/date';
 import { addWorkout } from '../../../api/workouts';
 import { Workout } from '../../../types';
-import { today, getLocalTimeZone, toCalendarDateTime } from '@internationalized/date';
+import { CalendarPicker } from './CalendarPicker';
 
 const AddForm = () => {
   const { control, register, handleSubmit } = useForm<Workout>();
@@ -57,46 +41,17 @@ const AddForm = () => {
         name='date'
         render={({ field: { onChange } }) => {
           return (
-            <DatePicker
-              onChange={(value) => onChange(toCalendarDateTime(value).toDate(getLocalTimeZone()))}
+            <CalendarPicker
+              onChange={(value) => {
+                onChange(toCalendarDateTime(value).toDate(getLocalTimeZone()));
+                console.log(value);
+              }}
               defaultValue={today(getLocalTimeZone())}
               maxValue={today(getLocalTimeZone())}
               name='date'
+              label='Date'
               isRequired
-              className='group flex flex-col gap-1 w-4/5'
-            >
-              <Label className='ml-6 text-slate-100'>Date</Label>
-              <Group className='flex mb-2 mx-4'>
-                <DateInput className='flex px-2 py-2 bg-zinc-900 text-slate-100 rounded-md outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300'>
-                  {(segment) => (
-                    <DateSegment
-                      className='rounded ml-1 outline-none focus:bg-green-300 focus:text-zinc-900'
-                      segment={segment}
-                    />
-                  )}
-                </DateInput>
-                <Button className='ml-4 text-slate-100'>▼</Button>
-              </Group>
-              <Popover>
-                <Dialog>
-                  <Calendar className='p-4 pb-6 mb-10 bg-zinc-800 rounded-3xl border border-black text-left text-slate-100 '>
-                    <header className='flex justify-evenly mb-4'>
-                      <Button slot='previous'>◀</Button>
-                      <Heading />
-                      <Button slot='next'>▶</Button>
-                    </header>
-                    <CalendarGrid>
-                      {(date) => (
-                        <CalendarCell
-                          className='w-10 h-10 cursor-default rounded-full flex items-center justify-center outside-month:text-gray-300 hover:bg-green-300 pressed:bg-gray-200 selected:bg-violet-700 hover:text-zinc-900 focus-visible:ring'
-                          date={date}
-                        />
-                      )}
-                    </CalendarGrid>
-                  </Calendar>
-                </Dialog>
-              </Popover>
-            </DatePicker>
+            />
           );
         }}
       />
