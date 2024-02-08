dfa864596578909cbdc81422d29deb2e1a7cb899
Petri <peewter@gmail.com>
2024-02-08 13:04:59 +0200

    Update calendar locale and style

--- src/components/Workout/Forms/CalendarPicker.tsx
+++ src/components/Workout/Forms/CalendarPicker.tsx
@@ -13,7 +13,9 @@ import {
   Label,
   Popover,
 } from 'react-aria-components';
+import { I18nProvider } from 'react-aria';
 import type { DatePickerProps, DateValue, ValidationResult } from 'react-aria-components';
+import { CalendarDaysIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 interface CalendarPickerProps<T extends DateValue> extends DatePickerProps<T> {
   label?: string;
@@ -27,39 +29,50 @@ export function CalendarPicker<T extends DateValue>({
   ...props
 }: CalendarPickerProps<T>) {
   return (
-    <DatePicker {...props} className='group flex flex-col gap-1 w-4/5'>
-      <Label className='ml-6 text-slate-100'>{label}</Label>
-      <Group className='flex mb-2 mx-4'>
-        <DateInput className='flex px-2 py-2 bg-zinc-900 text-slate-100 rounded-md outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300'>
-          {(segment) => (
-            <DateSegment
-              className='rounded ml-1 outline-none focus:bg-green-300 focus:text-zinc-900'
-              segment={segment}
-            />
-          )}
-        </DateInput>
-        <Button className='ml-4 text-slate-100'>▼</Button>
-      </Group>
-      <FieldError>{errorMessage}</FieldError>
-      <Popover>
-        <Dialog>
-          <Calendar className='p-4 pb-6 mb-10 bg-zinc-800 rounded-3xl border border-black text-left text-slate-100 '>
-            <header className='flex justify-evenly mb-4'>
-              <Button slot='previous'>◀</Button>
-              <Heading />
-              <Button slot='next'>▶</Button>
-            </header>
-            <CalendarGrid>
-              {(date) => (
-                <CalendarCell
-                  className='w-10 h-10 cursor-default rounded-full flex items-center justify-center outside-month:text-gray-300 hover:bg-green-300 pressed:bg-gray-200 selected:bg-violet-700 hover:text-zinc-900 focus-visible:ring'
-                  date={date}
-                />
-              )}
-            </CalendarGrid>
-          </Calendar>
-        </Dialog>
-      </Popover>
-    </DatePicker>
+    <I18nProvider locale='en-GB'>
+      <DatePicker {...props} className='group flex flex-col gap-1 w-full'>
+        <Label className='ml-6 text-slate-100'>{label}</Label>
+        <Group className='flex mb-2 mx-4'>
+          <DateInput className='flex px-2 py-2 bg-zinc-900 text-slate-100 rounded-md outline outline-1  outline-slate-100/50 w-full focus:outline-none focus:ring focus:ring-green-300'>
+            {(segment) => (
+              <DateSegment
+                className='rounded ml-1 outline-none focus:bg-green-300 focus:text-zinc-900'
+                segment={segment}
+              />
+            )}
+          </DateInput>
+          <Button className='ml-4 text-slate-100'>
+            <CalendarDaysIcon className='h-10 w-10' />
+          </Button>
+        </Group>
+        <FieldError>{errorMessage}</FieldError>
+        <Popover>
+          <Dialog>
+            <Calendar
+              className='p-4 mb-10 mr-3 bg-zinc-900 border border-zinc-900 rounded-xl 
+         text-slate-100'
+            >
+              <header className='flex justify-evenly mb-4'>
+                <Button slot='previous'>
+                  <ChevronLeftIcon className='h-8 w-8' />
+                </Button>
+                <Heading />
+                <Button slot='next'>
+                  <ChevronRightIcon className='h-8 w-8' />
+                </Button>
+              </header>
+              <CalendarGrid>
+                {(date) => (
+                  <CalendarCell
+                    className='w-10 h-10 cursor-default rounded-full flex items-center justify-center data-[disabled]:text-gray-500 hover:bg-green-300 hover:text-zinc-900  data-[disabled]:hover:bg-red-500 data-[outside-month]:hidden'
+                    date={date}
+                  />
+                )}
+              </CalendarGrid>
+            </Calendar>
+          </Dialog>
+        </Popover>
+      </DatePicker>
+    </I18nProvider>
   );
 }
