f194b2daa8154c92a6b3e40f5f2aac37b4e3e685
Petri <peewter@gmail.com>
2024-02-19 19:50:22 +0200

    Implement Totals component with date range picker

---
+++ src/components/Workout/Totals.tsx
@@ -0,0 +1,11 @@
+import { CalendarRangePicker } from './CalendarRangePicker';
+
+const TotalsList = () => {
+  return (
+    <>
+      <p>Workout dates</p>
+      <CalendarRangePicker aria-label='Workout dates' />
+    </>
+  );
+};
+export default TotalsList;
