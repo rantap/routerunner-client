126b8ef35880aac854b911d20c38460815ab4c2a
Petri <peewter@gmail.com>
2024-02-26 19:28:27 +0200

    Move add workout button to all workouts

--- src/components/Workouts/Tabs.tsx
+++ src/components/Workouts/Tabs.tsx
@@ -1,28 +1,32 @@
 import { Tab, TabList, TabPanel, Tabs } from 'react-aria-components';
 import CardList from './Cards/CardList';
 import Totals from './Totals';
+import AddModal from './Modals/AddModal';
 
 const WorkoutTabs = () => {
   return (
     <Tabs className='text-slate-300'>
-      <TabList className='flex justify-around mb-4 ' aria-label='Workouts'>
+      <TabList className='flex justify-around mb-2 border-b border-zinc-800' aria-label='Workouts'>
         <Tab
-          className='p-2 border-solid border-2 border-transparent outline-none hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100 data-[focus-visible]:outline-2 data-[focus-visible]:outline-slate-100 data-[focus-visible]:outline-solid'
+          className='p-2 border-solid border-2 border-transparent outline-none hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100 data-[focus-visible]:ring data-[focus-visible]:ring-orange-300'
           id='All'
         >
           All workouts
         </Tab>
         <Tab
-          className='p-2 border-solid border-2 border-transparent outline-none hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100 data-[focus-visible]:outline-2 data-[focus-visible]:outline-slate-100 data-[focus-visible]:outline-solid'
+          className='p-2 border-solid border-2 border-transparent outline-none hover:cursor-pointer data-[selected]:border-b-green-300 data-[selected]:text-slate-100 data-[focus-visible]:ring data-[focus-visible]:ring-orange-300'
           id='Totals'
         >
           Totals
         </Tab>
       </TabList>
       <TabPanel id='All'>
+        <div className='flex justify-center mb-8 text-zinc-900'>
+          <AddModal />
+        </div>
         <CardList />
       </TabPanel>
-      <TabPanel id='Totals' className=''>
+      <TabPanel id='Totals' className='mt-6'>
         <Totals />
       </TabPanel>
     </Tabs>
--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,4 +1,3 @@
-import AddModal from '../components/Workouts/Modals/AddModal';
 import Tabs from '../components/Workouts/Tabs';
 
 const Workouts = () => {
@@ -8,9 +7,6 @@ const Workouts = () => {
         <h1 className='mt-6 text-center text-slate-100 text-2xl font-bold tracking-tighter sm:mb-6'>
           My workouts
         </h1>
-      </div>
-      <div className='flex justify-center'>
-        <AddModal />
       </div>
       <div className='flex flex-wrap'>
         <div className='mx-auto mt-4 w-5/6 sm:w-3/5'>
